83a6b872f57141671029455c20c260400ca2be69
Nathaniel Cuenca <ncuenca.nac@gmail.com>
2021-07-21 10:40:53 -0700

    made UpdateUserForm

--- forms.py
+++ forms.py
@@ -23,3 +23,13 @@ class LoginForm(FlaskForm):
 
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[Length(min=6)])
+
+class UpdateUserForm(FlaskForm):
+    """Update User form"""
+
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    image_url = StringField('Image URL')
+    header_image_url = StringField('Header Image URL')
+    bio = StringField('Bio')
+    password = PasswordField('Password', validators=[Length(min=6)])
